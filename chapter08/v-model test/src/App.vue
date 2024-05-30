13e8ec890a19a19af2e963a2e4352014a831ba5e
Jjini <s_nearndear@naver.com>
2024-05-30 19:29:08 +0900

    Vue 커밋7-1

---
+++ chapter08/v-model test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 자식의 name을 searchName으로 전달하고 다시 자식의 변경값을 받아옴 -->
+    <!-- <InputName :name="searchName" @update:name="changdSearchName"/> 아래 코드로 생략 가능  -->
+    <InputName v-model:name="searchName" />
+    <h3>검색어: {{ searchName }}</h3>
+  </div>
+</template>
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: { InputName },
+  data() {
+    return {
+      searchName: 'John',
+    };
+  },
+  methods: {
+    // v-model로 양방향 바인딩시 생략 가능
+    changeSearchName(name) {
+      console.log(name);
+      this.searchName = name;
+    },
+  },
+};
+</script>
